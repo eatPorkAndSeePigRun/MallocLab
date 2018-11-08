bd7c7288f48178dcec1a09680233752bf53270d9
eatPorkAndSeePigRun <811302964@qq.com>
2018-11-08 14:50:14 +0800

    隐式空闲链表

--- src/mm.c
+++ src/mm.c
@@ -44,11 +44,33 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+static void *extend_heap(size_t words);
+static void *coalesce (void *bp);
+static void *find_fit (size_t asize); 
+static void place (void *bp, size_t asize);
+
+static void *heap_listp;
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    // 从系统得到4个字
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) {
+        return -1;
+    }
+    // 初始化
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    // 序言块头部
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    // 序言块脚部
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        // 结尾块头部
+    // 创建一个空的空闲链表
+    heap_listp += (2*WSIZE);
+    // 将堆扩展CHUNKSIZE字节，并且创建初始的空闲块
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) {
+        return -1;
+    }
     return 0;
 }
 
@@ -58,14 +80,42 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+/*
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
-	return NULL;
+	    return NULL;
     else {
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
     }
+*/
+
+    size_t asize; 
+    size_t extendsize;
+    char *bp;
+    // 检查请求真假
+    if (size == 0) {
+        return NULL;
+    }
+    // 调整请求块的大小
+    if (size <= DSIZE) {
+        asize = 2 * DSIZE;
+    } else {
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+    }
+    // 搜索空闲链表，寻找一个合适的空闲块，放置请求块后，分割出多余部分，后返回新地址
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+    // 用新空闲块扩展堆，放置请求块后，分割出多余部分，后返回新地址
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL) {
+        return NULL;
+    }
+    place(bp, asize);
+    return bp;
 }
 
 /*
@@ -73,6 +123,10 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(ptr));
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
 }
 
 /*
@@ -95,16 +149,81 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
+/*
+ * extend_heap函数在堆被初始化，或mm_malloc不能找到一个合适的匹配块时调用。
+ */
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+    // 将请求大小向上舍入为最接近的2字（8字节）的倍数，然后向系统请求额外堆空间
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) {
+        return NULL;
+    }
+    // 返回指向合并后的块的块指针
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
+    return coalesce(bp);
+}
 
+/*
+ * coalesce函数
+ */
+static void *coalesce (void *bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
 
+    if (prev_alloc && next_alloc) {
+        return bp;
+    } else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    } else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    } else {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
 
+/*
+ * 对隐式空闲链表执行首次适配搜索
+ */
+static void *find_fit (size_t asize) {
+    void *bp;
+    
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+    return NULL;
+}
 
+/*
+ * 将请求块放置在空闲块的起始位置，只有当剩余部分的大小等于或者超出最小块的大小时，才进行分割
+ */
+static void place (void *bp, size_t asize) {
+    size_t csize = GET_SIZE(HDRP(bp));
 
-
-
-
-
-
-
-
+    if ((csize - asize) >= (2*DSIZE)) {     // 分割后剩下的块大于或等于最小块大小
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);                 // 放置新的已分配块
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+    } else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
--- src/mm.h
+++ src/mm.h
@@ -21,3 +21,24 @@ typedef struct {
 
 extern team_t team;
 
+
+/* 基本常数大小 */
+#define WSIZE   4   /* 字大小 */
+#define DSIZE   8   /* 双字大小 */
+#define CHUNKSIZE   (1<<12) /* 初始块大小和扩展堆时的默认大小 */
+
+#define MAX(x,y)    ((x) > (y)? (x) : (y))
+
+#define PACK(size,alloc) ((size)|(alloc))   /* 将大小和已分配位结合并返回一个值，可把它存放在头部或者脚部中 */
+
+#define GET(p)      (*(unsigned int *)(p))          /* 读取和返回参数p引用的字 */
+#define PUT(p,val)  (*(unsigned int *)(p) = (val))  /* 将val存放在参数p指向的字中*/
+
+#define GET_SIZE(p)     (GET(p) & -0x7) /* 从p处的头部或者脚部分别返回大小 */
+#define GET_ALLOC(p)    (GET(p) & 0x1)  /* 从p处的头部或者脚部分别返回已分配位 */
+
+#define HDRP(bp)    ((char *)(bp) - WSIZE)  /* 返回指向块的头部 */
+#define FTRP(bp)    ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) /* 返回指向块的脚部 */
+
+#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))   /* 返回指向后面的块的块指针 */
+#define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))   /* 返回指向前面的块的块指针 */
